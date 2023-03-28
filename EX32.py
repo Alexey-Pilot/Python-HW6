9e27d7876b47118e096c3f79a1ada1cc864845ab
Alexey <alexragnarek@gmail.com>
2023-03-28 16:26:56 +0500

    ex32 is complited

---
+++ EX32.py
@@ -0,0 +1,23 @@
+'''Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+(т.е. не меньше заданного минимума и не больше заданного максимума)'''
+from random import randint
+def create_list(length):
+    res = []
+    for i in range(length):
+        res.append(randint(-20, 20))
+    return res
+def find_index(lst, nrange):
+    index_list = []
+    for i in range(len(lst)):
+        if nrange[0] <= lst[i] <= nrange[1]:
+            index_list.append(i)
+    return index_list
+
+
+
+
+size = int(input("Введите размер списка: "))
+num_range = [int(input(f"Введите {['минимальное', 'максимальное'][i]} число диапазона: ")) for i in range(2)]
+nums = create_list(size)
+print(nums)
+print(find_index(nums, num_range))
